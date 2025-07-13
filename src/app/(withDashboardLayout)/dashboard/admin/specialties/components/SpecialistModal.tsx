1e687be90827769dbf806fa3a00f19dcaa652b91
rabby4 <mdrabbybd4747@gmail.com>
2025-07-14 02:58:44 +0600

    feat(specialty): implement createSpecialty mutation with form integration

--- src/app/(withDashboardLayout)/dashboard/admin/specialties/components/SpecialistModal.tsx
+++ src/app/(withDashboardLayout)/dashboard/admin/specialties/components/SpecialistModal.tsx
@@ -1,9 +1,13 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import ProFileUploader from "@/components/form/ProFileUploader"
 import ProForm from "@/components/form/ProForm"
 import ProInput from "@/components/form/ProInput"
 import ProModal from "@/components/shared/ProModal/ProModal"
+import { useCreateSpecialtyMutation } from "@/redux/api/specialtiesApi"
+import { modifyPayload } from "@/utils/modifyPayload"
 import { Button, Grid } from "@mui/material"
 import { FieldValues } from "react-hook-form"
+import { toast } from "sonner"
 
 type TProps = {
 	open: boolean
@@ -11,8 +15,20 @@ type TProps = {
 }
 
 const SpecialistModal = ({ open, setOpen }: TProps) => {
-	const handleFormSubmit = (values: FieldValues) => {
-		console.log(values)
+	const [createSpecialty] = useCreateSpecialtyMutation()
+
+	const handleFormSubmit = async (values: FieldValues) => {
+		const data = modifyPayload(values)
+
+		try {
+			const res = await createSpecialty(data).unwrap()
+			if (res?.id) {
+				toast.success("Specialty created successfully!")
+				setOpen(false)
+			}
+		} catch (error: any) {
+			console.log(error.message)
+		}
 	}
 	return (
 		<ProModal open={open} setOpen={setOpen} title={"Create a new Specialty"}>
--- src/utils/modifyPayload.ts
+++ src/utils/modifyPayload.ts
@@ -1,8 +1,12 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 export const modifyPayload = (values: any) => {
 	const obj = { ...values }
+	const file = obj["file"]
+	delete obj["file"]
 	const data = JSON.stringify(obj)
 	const formData = new FormData()
 	formData.append("data", data)
+	formData.append("file", file as Blob)
+
 	return formData
 }
